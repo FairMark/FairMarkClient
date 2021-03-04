fbd649f9c39442fbb70554fea779337df2c2d75c
Alexey Yakovlev <yallie@yandex.ru>
2021-03-05 00:45:28 +0300

    Added credentials class.

---
+++ GisMtClient/Credentials.cs
@@ -0,0 +1,26 @@
+﻿namespace GisMtClient
+{
+    /// <summary>
+    /// GIS MT Credentials.
+    /// </summary>
+    public class Credentials
+    {
+        /// <summary>
+        /// Gets or sets the OMS Identity, taken from the user's profile,
+        /// see https://intuot.crpt.ru:12011/configuration/profile
+        /// </summary>
+        public string OmsID { get; set; }
+
+        /// <summary>
+        /// Gets or sets the device token, taken from the device properties,
+        /// see https://intuot.crpt.ru:12011/management/devices
+        /// </summary>
+        public string Connection { get; set; }
+
+        /// <summary>
+        /// Gets or sets the user identity, taken from the cryptographic certificate,
+        /// see Thumbprint property.
+        /// </summary>
+        public string UserID { get; set; }
+    }
+}
--- GisMtClient/MtClient.cs
+++ GisMtClient/MtClient.cs
@@ -1,8 +1,25 @@
 ﻿using System;
+using RestSharp;
 
 namespace GisMtClient
 {
+    /// <summary>
+    /// GIS MT REST API Client.
+    /// </summary>
     public class MtClient
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MtClient"/> class.
+        /// </summary>
+        /// <param name="baseUrl">Base API url.</param>
+        /// <param name="credentials">Credentials.</param>
+        public MtClient(string baseUrl, Credentials credentials)
+            : this(new RestClient(baseUrl), credentials)
+        {
+        }
+
+        public MtClient(IRestClient client, Credentials credentials)
+        {
+        }
     }
 }
