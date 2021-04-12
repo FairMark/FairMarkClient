f6b2fc6e73b4550fdf7bad44e5525a52b87fc962
Alexey Yakovlev <yallie@yandex.ru>
2021-04-12 11:55:10 +0300

    Added a comment.

--- TrueApi/TrueApi/TrueApiClient.Chapter5.cs
+++ TrueApi/TrueApi/TrueApiClient.Chapter5.cs
@@ -5,12 +5,14 @@
 
     public partial class TrueApiClient
     {
-        public string[] GetCisesByOrderId(string docNum)//TODO Сделать поддержку второго параметра pg (товарная группа)
+        /// <summary>
+        /// 6.9. Метод получения списка КИ по номеру документа.
+        /// </summary>
+        /// <param name="docNum"></param>
+        /// <returns>Список КИ</returns>
+        public string[] GetCisesByOrderId(string docNum) // TODO Сделать поддержку второго параметра pg (товарная группа)
         {
             return Get<string[]>("/cises/doc/{docNum}", new[] { new Parameter("docNum", docNum, ParameterType.UrlSegment) });
         }
-
-
-
     }
 }
