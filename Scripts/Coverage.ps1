8c2bbe5d2186dbf707e85f37846aee4e5d453d1c
Alexey Yakovlev <yallie@yandex.ru>
2021-04-13 18:56:09 +0300

    Working on CI integration.

---
+++ Scripts/Coverage.ps1
@@ -0,0 +1,21 @@
+﻿# run unit tests and gather coverage information
+$args = "test FairMark.Tests --logger=trx;LogFileName=TestResults.trx"
+$filter = "+[*]* -[*Test*]* -[*]MdlpApiClient.Xsd*"
+OpenCover.Console.exe -returntargetcode -register:administrator -target:dotnet.exe "-targetargs:$args" "-filter:$filter" -output:FairMarkCoverage.xml
+$exit = $lastexitcode
+
+# upload reports to the codecov.io server
+codecov -t "c01ca799-5c1f-4647-9743-7d6e8b3fb0f8" -f FairMarkCoverage.xml 
+
+# convert trx reports to JUnit format so Gitlab can parse them
+trx2junit FairMark.Tests\TestResults\TestResults.trx
+#& "$PSScriptRoot\TestReportPrefix.ps1" -inputFileName FairMark.Tests\TestResults\TestResults.xml -outputFileName FairMark.Tests\TestResults\TestResults.xml -prefix "Normal."
+
+# make sure we don't fail on REST methods that need long timeouts
+Wait-Event -Timeout 20
+
+# return dotnet test exit code
+if (($exit + $exit2) -ne 0)
+{
+	exit ($exit + $exit2)
+}
---
+++ Scripts/TestReportPrefix.ps1
@@ -0,0 +1,38 @@
+﻿# Add prefix to the test fixture names
+# This step is necessary because GitLab seems to
+# merge test reports based on their class names
+#
+# Usage:
+#
+# powershell /file TestReportPrefix.ps1 -inputFileName in.xml -outputFileName out.xml -prefix MyProject
+
+# Powershell 5.0 is required for using:
+#using assembly System.Xml.Linq
+#using namespace System.Xml.Linq
+
+param (
+	[parameter(Mandatory = $true)] $inputFileName, 
+	[parameter(Mandatory = $true)] $outputFileName, 
+	[parameter(Mandatory = $true)] $prefix
+)
+
+$xmlLinq = [System.Reflection.Assembly]::LoadWithPartialName("System.Xml.Linq");
+
+echo "Loading $inputFileName..."
+$xml = [System.Xml.Linq.XDocument]::Load($inputFileName)
+
+# add prefix to all test suite class names
+ForEach ($suite in $xml.Root.Elements("testsuite")) 
+{
+	ForEach ($case in $suite.Elements("testcase"))
+	{
+		$name = $case.Attribute("classname")
+		if ($name -ne $null)
+		{
+			$name.Value = $prefix + "." + $name.Value
+		}
+	}
+}
+
+echo "Saving $outputFileName..."
+$xml.Save($outputFileName)
